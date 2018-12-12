51d389df873395fb47966e97f70f15ea25e6681a
Joshua Ritter <jobblritter@web.de>
2018-12-12 20:58:55 +0100

    Test der Spielfeld-Funktionen

--- SE1Projekt/src/Spiel.java
+++ SE1Projekt/src/Spiel.java
@@ -1,18 +1,50 @@
+import java.util.*;
 
 public class Spiel {
 	
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
 		Spieler firstPlayer = new Spieler();
 		Spieler secondPlayer = new Spieler();
 		Spielfeld spielbrett = new Spielfeld();
 		spielbrett.platziereObjekte(firstPlayer.getFiguren(), secondPlayer.getFiguren());
-		System.out.println(spielbrett.spielfeld[0][0].toString());
-		System.out.println(spielbrett.spielfeld[2][0].toString());
-		System.out.println(spielbrett.spielfeld[4][0].toString());
-		System.out.println(spielbrett.spielfeld[6][0].toString());
-		System.out.println(spielbrett.spielfeld[8][0].toString());
+	
 		
+		while(true) {
 		spielbrett.printSpielfeld();
+		System.out.println("Spieler 1 ist an der Reihe.\n");
+		
+	
+		
+		System.out.println("Waehlen sie eine Figur per Koordinate.\n");
+		int k1 = sc.nextInt();
+		int k2 = sc.nextInt();
+		spielbrett.waehleFigur(new Koordinate(k1, k2), firstPlayer);
+		System.out.println(spielbrett.getGewaehlteFigur().toString());
+		
+		
+		
+		System.out.println("Wohin wollen Sie ziehen?\n");
+		k1 = sc.nextInt();
+		k2 = sc.nextInt();
+		spielbrett.bewegeFigur(new Koordinate(k1, k2));
+		
+		if(firstPlayer.getFiguren().get(1).isWurdeBewegt())
+			System.out.println("Wurde gewaehlt");
+		
+		System.out.println("Spieler 2 ist an der Reihe.\n");
+		
+		System.out.println("Waehlen sie eine Figur per Koordinate.\n");
+		k1 = sc.nextInt();
+		k2 = sc.nextInt();
+		spielbrett.waehleFigur(new Koordinate(k1, k2), secondPlayer);
+		
+		System.out.println("Wohin wollen Sie ziehen?\n");
+		k1 = sc.nextInt();
+		k2 = sc.nextInt();
+		spielbrett.bewegeFigur(new Koordinate(k1, k2));
+		
+		}
 	}
 }
